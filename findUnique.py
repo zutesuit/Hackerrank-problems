3f0e4dc3d14029fb00c76dd819a5828cbacb00b1
zutesuit <51677761+zutesuit@users.noreply.github.com>
2023-07-05 19:12:49 +0100

    Create findUnique.py

---
+++ findUnique.py
@@ -0,0 +1,36 @@
+# Given an array of integers, where all elements but one occur twice, find the unique element.
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'lonelyinteger' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY a as parameter.
+#
+
+def lonelyinteger(a):
+    # Write your code here
+    unique = 0
+    for num in a:
+        unique ^= num # this puts all elements thorugh XOr, all duplicates cancel out
+    return unique
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    a = list(map(int, input().rstrip().split()))
+
+    result = lonelyinteger(a)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
